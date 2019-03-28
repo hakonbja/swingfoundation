62c94da93d69fb9a246f8d1616ffa9885e4a9cbf
Hakon <32593278+hakonbja@users.noreply.github.com>
2019-03-29 00:18:21 +0100

    Overview redesigned with cards

--- wp-content/themes/swifo_theme/page-news.php
+++ wp-content/themes/swifo_theme/page-news.php
@@ -1,22 +1,77 @@
 
 <?php get_header();?>
-<div class="content news-page">
-  <section>
-      <div class="container events">
-        <div class="hide-overflow-x">
-          <h3 class="orange-line">Events</h3>
-        </div>
-        <h4>Our latest events!</h4>
+<div class="content news-page container">
+
+<div class="cards grid">
+
+  <a class="card" href="#">
+    <div class="card__figure">
+      <figure>
+        <img src="https://picsum.photos/350/400?random" alt="">
+      </figure>
+      <div class="card__meta">
+        <p class="type">Story</p>
+        <p class="date">28.03.2019</p>
       </div>
-  </section>
-  <section>
-      <div class="container stories">
-        <div class="hide-overflow-x">
-          <h3 class="orange-line">Stories</h3>
-        </div>
-        <h4>The freshest stories!</h4>
+    </div>
+    <div class="card__info">
+      <h4 class="title">Savoy Cup</h4>
+      <p class="excerpt">We went to compete in one of the biggest Lindy Hop competitions in Europe!</p>
+    </div>
+  </a>
+
+  <a class="card" href="#">
+    <div class="card__figure">
+      <figure>
+        <img src="https://picsum.photos/350/300?random" alt="">
+      </figure>
+      <div class="card__meta">
+        <p class="type">Event</p>
+        <p class="date">11.04.2019</p>
       </div>
-  </section>
+    </div>
+    <div class="card__info">
+      <h4 class="title">Solo workshop</h4>
+      <p class="excerpt">On the 14th of May you can...</p>
+    </div>
+  </a>
+
+  <a class="card" href="#">
+    <div class="card__figure">
+      <figure>
+        <img src="https://picsum.photos/350/?random" alt="">
+      </figure>
+      <div class="card__meta">
+        <p class="type">Story</p>
+        <p class="date">8.05.2019</p>
+      </div>
+    </div>
+    <div class="card__info">
+      <h4 class="title">Lindy Hop in the Netherlands</h4>
+      <p class="excerpt">How it all began...</p>
+    </div>
+  </a>
+
+  <a class="card" href="#">
+    <div class="card__figure">
+      <figure>
+        <img src="https://picsum.photos/400/?random" alt="">
+      </figure>
+      <div class="card__meta">
+        <p class="type">Event</p>
+        <p class="date">18.06.2019</p>
+      </div>
+    </div>
+    <div class="card__info">
+      <h4 class="title">Summer class</h4>
+      <p class="excerpt">Learn how to do the Didlibum!</p>
+    </div>
+  </a>
+
+</div>
+
+
+
 </div>
   <?php get_footer();?>
 </body>
