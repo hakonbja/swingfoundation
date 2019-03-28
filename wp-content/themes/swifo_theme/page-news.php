b3333620c1d4d0c225678e50c068ce4002128cf1
Hakon <32593278+hakonbja@users.noreply.github.com>
2019-03-28 20:52:31 +0100

    First commit

---
+++ wp-content/themes/swifo_theme/page-news.php
@@ -0,0 +1,22 @@
+
+<?php get_header();?>
+<div class="content news-page">
+  <section>
+      <div class="container events">
+        <div class="hide-overflow-x">
+          <h3 class="orange-line">Events</h3>
+        </div>
+        <h4>Our latest events!</h4>
+      </div>
+  </section>
+  <section>
+      <div class="container stories">
+        <div class="hide-overflow-x">
+          <h3 class="orange-line">Stories</h3>
+        </div>
+        <h4>The freshest stories!</h4>
+      </div>
+  </section>
+</div>
+  <?php get_footer();?>
+</body>
