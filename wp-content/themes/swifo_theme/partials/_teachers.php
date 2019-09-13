0b9b25a74e8f4f2fedabf0c23bfdbca1327311cf
Hakon <32593278+hakonbja@users.noreply.github.com>
2019-09-13 18:01:38 +0200

    finish teachers template

--- wp-content/themes/swifo_theme/partials/_teachers.php
+++ wp-content/themes/swifo_theme/partials/_teachers.php
@@ -4,6 +4,7 @@
 <?php
 $args = array(
   'post_type' => 'teachers',
+  'orderby' => 'title',
   'order' => 'ASC',
 );
 $teachers = new WP_Query( $args );
@@ -12,11 +13,14 @@ $content = get_the_content();
 $content = wp_filter_nohtml_kses($content);
 ?>
   <div class="teacher">
+  <?php if (has_post_thumbnail()) : ?>
     <div class="img-container">
       <?php the_post_thumbnail() ?>
       <?php $title = get_post(get_post_thumbnail_id()) -> post_title; ?>
       <p class="subtext"><?php echo $title ?></p>
     </div>
+  <?php endif ?>
+
     <div class="text-container">
       <h4><?php the_title() ?></h4>
       <?php the_content() ?>
