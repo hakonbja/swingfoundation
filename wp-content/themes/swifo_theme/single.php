5e40b55e8aff9e3e031bc200d509aae6ce20e203
Hakon <32593278+hakonbja@users.noreply.github.com>
2019-10-05 00:51:08 +0200

    change link back to Stories

--- wp-content/themes/swifo_theme/single.php
+++ wp-content/themes/swifo_theme/single.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all single posts and attachments
+ * The template for displaying all single posts and attachments - In our case the "Stories"
  *
  */
 
@@ -45,15 +45,15 @@ get_header(); ?>
       endwhile;
     ?>
 
-    <!-- <div class="link-back">
-      <a href="<?php echo get_site_url(); ?>/news">
+    <div class="link-back">
+      <a href="<?php echo get_site_url(); ?>/stories">
         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-double-left" class="svg-inline--fa fa-angle-double-left fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
         <path
           d="M223.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L319.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L393.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34zm-192 34l136 136c9.4 9.4 24.6 9.4 33.9 0l22.6-22.6c9.4-9.4 9.4-24.6 0-33.9L127.9 256l96.4-96.4c9.4-9.4 9.4-24.6 0-33.9L201.7 103c-9.4-9.4-24.6-9.4-33.9 0l-136 136c-9.5 9.4-9.5 24.6-.1 34z"></path>
         </svg>
-        Back to News
+        Back to Stories
       </a>
-    </div> -->
+    </div>
 
   </div>
 </div>
