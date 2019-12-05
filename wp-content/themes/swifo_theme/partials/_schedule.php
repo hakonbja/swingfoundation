b02bfadad23c8cad42d2c3e65418b882dc518adf
Hákon <hakonbja@gmail.com>
2019-12-05 11:39:02 +0100

    add schedule for 2020 and a year sign for tabs

--- wp-content/themes/swifo_theme/partials/_schedule.php
+++ wp-content/themes/swifo_theme/partials/_schedule.php
@@ -9,11 +9,12 @@
 
     <div class="tabs">
       <ul>
-        <li class="tab-1 disabled">Block 1</li>
-        <li class="tab-2">Block 2</li>
+        <!-- <li class="tab-1 disabled">Block 1</li> -->
+        <li class="tab-2">Block 2<span class="year">2019</span></li>
         <li class="tab-3">Block 3</li>
         <li class="tab-4">Block 4</li>
-        <li class="tab-5 selected">Block 5</li>
+        <li class="tab-5">Block 5</li>
+        <li class="tab-6 selected">Block 1<span class="year">2020</span></li>
       </ul>
     </div>
     
@@ -34,7 +35,10 @@
     <div class="schedule__wrapper content-5 selected">
       <?php get_template_part( 'partials/_schedule__block-5'); ?>
     </div>
+
+    <div class="schedule__wrapper content-6 selected">
+      <?php get_template_part( 'partials/_schedule__block-1-2020'); ?>
+    </div>
     
-  <div class="schedule__wrapper content-5"></div>
 
 </div>
---
+++ wp-content/themes/swifo_theme/partials/_schedule__block-1-2020.php
@@ -0,0 +1,79 @@
+<table class="schedule">
+  <thead>
+    <tr>
+      <th></th>
+      <th>Room 1</th>
+      <th>Room 2</th>
+      <th>Room 3</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>18:30-</td>
+      <td rowspan="2">Lindy 5</td>
+      <td rowspan="2">Lindy 1</td>
+      <td rowspan="2">Solo Beg</td>
+    </tr>
+    <tr>
+      <td>19:45</td>
+    </tr>
+  </tbody>
+  <tbody>
+    <tr>
+      <td>19:50-</td>
+      <td rowspan="2">Lindy 7</td>
+      <td rowspan="2">Lindy 3</td>
+      <td rowspan="2">Solo Adv</td>
+    </tr>
+    <tr>
+      <td>21:05</td>
+    </tr>
+    <tr>
+      <td>21:05-</td>
+      <td rowspan="2">Lindy 2</td>
+      <td rowspan="2">Lindy 8</td>
+      <td rowspan="2">Solo Int</td>
+    </tr>
+    <tr>
+      <td>22:20</td>
+    </tr>
+  </tbody>
+</table>
+
+<div class="dates-wrapper-block-4">
+<table class="dates">
+    <thead>
+      <tr>
+        <th colspan="2">Dates for Room 1 & 2</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>January:</td>
+        <td>14, 21, 28</td>
+      </tr>
+      <tr>
+        <td>February:</td>
+        <td>4, 11, 18, 25</td>
+      </tr>
+    </tbody>
+  </table>
+  
+  <table class="dates">
+    <thead>
+      <tr>
+        <th colspan="2">Dates for Room 3</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>January:</td>
+        <td>14, 28</td>
+      </tr>
+      <tr>
+        <td>February:</td>
+        <td>4, 11, 18, 25</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
